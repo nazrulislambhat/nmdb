0d632152c68f7534767a24883eb746272fe1520b
Nazrul Islam <nazrulislambhat@gmail.com>
2023-08-17 11:37:41 +0530

    Header Component

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import Logo from '../Header/Logo';
+
+export default function Header() {
+    return (
+        <>
+            <header className='header'>
+                <div className='header__logo'>
+                    <Logo />
+                </div>
+                <div className='header__nav'>
+
+                </div>
+            </header>
+        </>
+    )
+}
