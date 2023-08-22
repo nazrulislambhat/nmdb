c014ac787cbfd55ed1ad0ed135513f9c58a8f7f2
Nazrul Islam <nazrulislambhat@gmail.com>
2023-08-22 18:17:56 +0530

    footer

--- app/components/Footer/FooterNav.tsx
+++ app/components/Footer/FooterNav.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import footerData from './footerData.json';
 
-export default function FooterNav ()  {
-  const menus = Object.keys(footerData);
-
+export default function FooterNav() {
   return (
-    <footer className='flex'>
-      {menus.map((menuName) => (
+    <footer className="flex">
+      {Object.entries(footerData).map(([menuName, items]) => (
         <div key={menuName} className="menu px-10 text-white leading-6">
-          <h2 className='font-bold'>{menuName}</h2>
-          <ul className='font-normal text-sm leading-6'>
-            {footerData[menuName].map((item, index) => (
-              <li key={index}>
-                <Link href={item.link} className="hover:underline">
-                  {item.label}
-                </Link>
+          <h2 className="font-bold">{menuName}</h2>
+          <ul className="font-normal text-sm leading-6">
+            {items.map(({ label, link }) => (
+              <li key={label}>
+                <Link href={link} className='hover:underline'>{label}</Link>
               </li>
             ))}
           </ul>
@@ -23,5 +19,4 @@ export default function FooterNav ()  {
       ))}
     </footer>
   );
-};
-
+}
