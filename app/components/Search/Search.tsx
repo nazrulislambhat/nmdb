b205d8b8a65e8eb6752b5023bc4920d755aede40
Nazrul Islam <nazrulislambhat@gmail.com>
2023-09-01 20:42:21 +0530

    home search

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -10,7 +10,9 @@ import loadingIcon from '../../../public/loading.gif';
 export default function Search() {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [trendingData, setTrendingData] = useState<any[]>([]);
+  const [searchResults, setSearchResults] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [hasSearched, setHasSearched] = useState<boolean>(false);
 
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
@@ -18,7 +20,9 @@ export default function Search() {
 
   const handleClear = () => {
     setSearchQuery('');
+    setHasSearched(false);
   };
+
   useEffect(() => {
     const fetchTrendingData = async () => {
       try {
@@ -43,6 +47,34 @@ export default function Search() {
     const timeoutId = setTimeout(fetchTrendingData, 2000);
     return () => clearTimeout(timeoutId);
   }, []);
+
+  useEffect(() => {
+    if (searchQuery.trim() === '') {
+      setSearchResults([]);
+      setHasSearched(false);
+      return;
+    }
+
+    const fetchSearchResults = async () => {
+      try {
+        setIsLoading(true);
+        const searchEndpoint = `search/multi/?query=${searchQuery}`;
+        console.log(searchEndpoint);
+        const searchData = await fetchData(searchEndpoint);
+        console.log(searchData);
+        setSearchResults(searchData.results);
+        setIsLoading(false);
+        setHasSearched(true);
+      } catch (error) {
+        console.error('Error fetching search results:', error);
+        setIsLoading(false);
+        setHasSearched(true);
+      }
+    };
+
+    const timeoutId = setTimeout(fetchSearchResults, 1000);
+    return () => clearTimeout(timeoutId);
+  }, [searchQuery]);
 
   return (
     <div className="search-wrapper z-40 flex justify-center w-full flex-col absolute top-[57px] left-0">
@@ -74,7 +106,7 @@ export default function Search() {
             />
           )}
         </div>
-        <div className="trending-data">
+        <div className="search-results">
           <h2 className="text-2xl flex font-bold bg-gray-100 p-2">
             <Image
               src={trendingIcon}
@@ -83,26 +115,43 @@ export default function Search() {
               height={20}
               className="mr-1"
             />
-            Latest Trending
+            {hasSearched ? 'Search Results' : 'Latest Trending'}
           </h2>
-          <ul>
-            {!isLoading &&
-              trendingData.map((item: any) => (
-                <li
-                  key={item.id}
-                  className="border-b-[1px] flex last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
-                >
-                  <Image
-                    src={searchIcon}
-                    alt="Search Icon"
-                    width={15}
-                    height={15}
-                    className="mr-1"
-                  />
-                  <span className="">{item.title || item.name}</span>
-                </li>
-              ))}
-          </ul>
+          {!isLoading && (
+            <ul>
+              {hasSearched && searchResults
+                ? searchResults.map((item: any) => (
+                    <li
+                      key={item.id}
+                      className="border-b-[1px] flex last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
+                    >
+                      <Image
+                        src={searchIcon}
+                        alt="Search Icon"
+                        width={15}
+                        height={15}
+                        className="mr-1"
+                      />
+                      <span className="">{item.title || item.name}</span>
+                    </li>
+                  ))
+                : trendingData.map((item: any) => (
+                    <li
+                      key={item.id}
+                      className="border-b-[1px] flex last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
+                    >
+                      <Image
+                        src={searchIcon}
+                        alt="Search Icon"
+                        width={15}
+                        height={15}
+                        className="mr-1"
+                      />
+                      <span className="">{item.title || item.name}</span>
+                    </li>
+                  ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
