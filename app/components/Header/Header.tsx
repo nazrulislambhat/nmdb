a574d0941c765f3c4d4d7a4e205caa55936f5c5d
Nazrul Islam <nazrulislambhat@gmail.com>
2023-09-01 12:37:34 +0530

    home search

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,14 +1,23 @@
+'use client';
+import React, { useState } from 'react';
 import Logo from '../Header/Logo';
-import React from 'react';
 import Navigation from './Navigation';
 import menuData from './menuData.json';
 import Image from 'next/image';
 import searchIcon from '../../../public/search.svg';
+import closeIcon from '../../../public/close.svg';
+import Search from '../Search/Search';
 
 export default function Header() {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
   return (
     <div className="header--wrapper px-4 py-2 sm:px-28 sm:py-4 bg-backgroundColor">
-      <header className="header flex flex-start  max-w-[1300px] mx-auto flex-row items-center justify-start px-10 py-0">
+      <header className="header flex flex-start max-w-[1300px] mx-auto flex-row items-center justify-between px-10 py-0">
         <div className="flex items-center">
           <div className="header--logo pr-16">
             <Logo />
@@ -17,15 +26,23 @@ export default function Header() {
             <Navigation menuData={menuData} />
           </div>
         </div>
-        <div className="header--search relative cursor-pointer">
-          <Image
-            src={searchIcon}
-            alt="Header Search Icon"
-            width={20}
-            height={20}
-          />
+        <div
+          className="header--search relative cursor-pointer"
+          onClick={toggleSearch}
+        >
+          {isSearchOpen ? (
+            <Image src={closeIcon} alt="Close Icon" width={25} height={25} />
+          ) : (
+            <Image
+              src={searchIcon}
+              alt="Header Search Icon"
+              width={20}
+              height={20}
+            />
+          )}
         </div>
       </header>
+      {isSearchOpen && <Search />}
     </div>
   );
 }
---
+++ app/components/Search/Search.tsx
@@ -0,0 +1,110 @@
+'use client';
+import { useState, ChangeEvent, useEffect } from 'react';
+import clearIcon from '../../../public/clear.svg';
+import Image from 'next/image';
+import { fetchData } from '../../utils/api'; // Import your fetchData function
+import searchIcon from '../../../public/search-black.svg';
+import trendingIcon from '../../../public/trending.svg';
+import loadingIcon from '../../../public/loading.gif';
+
+export default function Search() {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [trendingData, setTrendingData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleClear = () => {
+    setSearchQuery('');
+  };
+  useEffect(() => {
+    const fetchTrendingData = async () => {
+      try {
+        const moviesData = await fetchData('trending/movie/week');
+        const tvShowsData = await fetchData('trending/tv/week');
+
+        const combinedData = [
+          ...moviesData.results,
+          ...tvShowsData.results,
+        ].sort((a, b) => b.popularity - a.popularity);
+
+        const latest10Data = combinedData.slice(0, 10);
+
+        setTrendingData(latest10Data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching trending data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    const timeoutId = setTimeout(fetchTrendingData, 2000);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  return (
+    <div className="search-wrapper z-40 flex justify-center w-full flex-col absolute top-[57px] left-0">
+      <div className="search-container w-screen max-w-7xl border-b-2 mx-auto bg-white border-gray-300 px-4 py-1">
+        <div className="flex justify-center items-center">
+          <Image src={searchIcon} alt="Search Icon" width={20} height={20} />
+          <input
+            type="text"
+            placeholder="Search for a movie, tv show, person..."
+            value={searchQuery}
+            onChange={handleSearchChange}
+            className="flex-grow py-2 px-4  focus-visible:outline-0"
+          />
+          {isLoading ? (
+            <Image
+              src={loadingIcon}
+              alt="Loading Icon"
+              width={20}
+              height={20}
+            />
+          ) : (
+            <Image
+              src={clearIcon}
+              alt="Clear Icon"
+              width={20}
+              height={20}
+              onClick={handleClear}
+              className="cursor-pointer"
+            />
+          )}
+        </div>
+        <div className="trending-data">
+          <h2 className="text-2xl flex font-bold bg-gray-100 p-2">
+            <Image
+              src={trendingIcon}
+              alt="Trending Icon"
+              width={20}
+              height={20}
+              className="mr-1"
+            />
+            Latest Trending
+          </h2>
+          <ul>
+            {!isLoading &&
+              trendingData.map((item: any) => (
+                <li
+                  key={item.id}
+                  className="border-b-[1px] flex last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
+                >
+                  <Image
+                    src={searchIcon}
+                    alt="Search Icon"
+                    width={15}
+                    height={15}
+                    className="mr-1"
+                  />
+                  <span className="">{item.title || item.name}</span>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
