5b472e35c5131df74219f023a5eaa1ae23c6e7e7
Nazrul Islam <nazrulislambhat@gmail.com>
2023-09-07 20:09:47 +0530

    show results only when user click inside search box

--- app/components/Search/Search.tsx
+++ app/components/Search/Search.tsx
@@ -17,6 +17,7 @@ export default function Search() {
   const [searchResults, setSearchResults] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [hasSearched, setHasSearched] = useState<boolean>(false);
+  const [isSearchBoxActive, setIsSearchBoxActive] = useState<boolean>(false); // Flag for search box activation
 
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
@@ -25,6 +26,10 @@ export default function Search() {
   const handleClear = () => {
     setSearchQuery('');
     setHasSearched(false);
+  };
+
+  const handleSearchBoxClick = () => {
+    setIsSearchBoxActive(true);
   };
 
   useEffect(() => {
@@ -114,8 +119,13 @@ export default function Search() {
 
   return (
     <div className="search-wrapper bg-white  z-40 flex justify-center w-full flex-col absolute top-[57px] left-0">
-      <div className="search-container w-screen max-w-screen border-b-2 px-[26%] bg-white border-gray-300 py-1">
-        <div className="flex justify-center items-center ">
+      <div className="search-container w-screen max-w-screen border-b-[1px] px-[26%] bg-white border-gray-300 py-1">
+        <div
+          className={`search-box flex justify-center items-center ${
+            isSearchBoxActive ? 'active' : ''
+          }`}
+          onClick={handleSearchBoxClick}
+        >
           <Image src={searchIcon} alt="Search Icon" width={15} height={15} />
           <input
             type="text"
@@ -142,51 +152,83 @@ export default function Search() {
             />
           )}
         </div>
-        <div className="search-results">
-          {!isLoading && (
-            <ul>
-              <h2 className="text-2xl flex items-center font-bold bg-gray-100 p-2">
-                <Image
-                  src={trendingIcon}
-                  alt="Trending Icon"
-                  width={20}
-                  height={20}
-                  className="mr-1"
-                />
-                {hasSearched ? 'Search Results' : 'Trending'}
-              </h2>
-              {hasSearched && searchResults
-                ? searchResults.map((item: any) => (
-                    <li
-                      key={item.id}
-                      className="border-b-[1px] flex items-center last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
-                      onClick={() => handleResultItemClick(item)}
-                    >
-                      {item.media_type === 'movie' ? (
-                        <Image
-                          src={movieIcon}
-                          alt="Movie Icon"
-                          width={15}
-                          height={10}
-                          className="mr-2 w-[15px] h-[15px]"
-                        />
-                      ) : item.media_type === 'person' ? (
-                        <Image
-                          src={personIcon}
-                          alt="Person Icon"
-                          width={15}
-                          height={10}
-                          className="mr-2 w-[15px] h-[15px]"
-                        />
-                      ) : item.media_type === 'tv' ? (
-                        <Image
-                          src={televisionIcon}
-                          alt="Television Icon"
-                          width={15}
-                          height={10}
-                          className="mr-2 w-[15px] h-[15px]"
-                        />
-                      ) : (
+        {isSearchBoxActive && (
+          <div className="search-results">
+            {!isLoading && (
+              <ul>
+                <h2 className="text-2xl flex items-center font-bold bg-gray-100 p-2">
+                  <Image
+                    src={trendingIcon}
+                    alt="Trending Icon"
+                    width={20}
+                    height={20}
+                    className="mr-1"
+                  />
+                  {hasSearched ? 'Search Results' : 'Trending'}
+                </h2>
+                {hasSearched && searchResults
+                  ? searchResults.map((item: any) => (
+                      <li
+                        key={item.id}
+                        className="border-b-[1px] flex items-center last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
+                        onClick={() => handleResultItemClick(item)}
+                      >
+                        {item.media_type === 'movie' ? (
+                          <Image
+                            src={movieIcon}
+                            alt="Movie Icon"
+                            width={15}
+                            height={10}
+                            className="mr-2 w-[15px] h-[15px]"
+                          />
+                        ) : item.media_type === 'person' ? (
+                          <Image
+                            src={personIcon}
+                            alt="Person Icon"
+                            width={15}
+                            height={10}
+                            className="mr-2 w-[15px] h-[15px]"
+                          />
+                        ) : item.media_type === 'tv' ? (
+                          <Image
+                            src={televisionIcon}
+                            alt="Television Icon"
+                            width={15}
+                            height={10}
+                            className="mr-2 w-[15px] h-[15px]"
+                          />
+                        ) : (
+                          <Image
+                            src={searchIcon}
+                            alt="Search Icon"
+                            width={15}
+                            height={15}
+                            className="mr-2 w-[15px] h-[15px]"
+                          />
+                        )}
+
+                        <div className="flex items-center">
+                          <span className="font-bold pr-1">
+                            {item.title || item.name}
+                          </span>{' '}
+                          in
+                          <span className="font-base pl-1">
+                            {item.media_type === 'movie'
+                              ? 'Movies'
+                              : item.media_type === 'tv'
+                              ? 'TV Shows'
+                              : item.media_type === 'person'
+                              ? 'People'
+                              : 'Unknown'}
+                          </span>
+                        </div>
+                      </li>
+                    ))
+                  : trendingData.map((item: any) => (
+                      <li
+                        key={item.id}
+                        className="border-b-[1px] flex items-center last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
+                      >
                         <Image
                           src={searchIcon}
                           alt="Search Icon"
@@ -194,45 +236,15 @@ export default function Search() {
                           height={15}
                           className="mr-2 w-[15px] h-[15px]"
                         />
-                      )}
-
-                      <div className="flex items-center">
-                        <span className="font-bold pr-1">
+                        <span className="font-base">
                           {item.title || item.name}
-                        </span>{' '}
-                        in
-                        <span className="font-base pl-1">
-                          {item.media_type === 'movie'
-                            ? 'Movies'
-                            : item.media_type === 'tv'
-                            ? 'TV Shows'
-                            : item.media_type === 'person'
-                            ? 'People'
-                            : 'Unknown'}
                         </span>
-                      </div>
-                    </li>
-                  ))
-                : trendingData.map((item: any) => (
-                    <li
-                      key={item.id}
-                      className="border-b-[1px] flex items-center last:border-b-[0px] px-2 py-[2px] hover:bg-gray-100 cursor-pointer"
-                    >
-                      <Image
-                        src={searchIcon}
-                        alt="Search Icon"
-                        width={15}
-                        height={15}
-                        className="mr-2 w-[15px] h-[15px]"
-                      />
-                      <span className="font-base">
-                        {item.title || item.name}
-                      </span>
-                    </li>
-                  ))}
-            </ul>
-          )}
-        </div>
+                      </li>
+                    ))}
+              </ul>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
